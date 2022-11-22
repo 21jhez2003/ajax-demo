3c27ae8bb2c8fdde024b95abb23b70f49366d7d0
raed <restrictedhorizon.10@gmail.com>
2022-11-22 09:51:21 +0800

    Added some comments

--- _includes/process.inc.php
+++ _includes/process.inc.php
@@ -11,10 +11,15 @@ if (isset($_POST['isClicked'])) {
     $first_name =  $_POST['first_name'];
     $last_name =  $_POST['last_name'];
 
-
+    /* 
+     The number of ? will depend on the number of your fields
+     I want to insert 5 data in 5 fields, this means that the number of my ? is 5
+    */
     if ($password === $confirm_password) {
-        $query = 'INSERT INTO users (username, password, email, first_name, last_name) VALUES (?,?,?,?,?)';
+        $query = 'INSERT INTO users (`username`, `password`, `email`, `first_name`, `last_name`) VALUES (?,?,?,?,?)';
         $result = $pdo->prepare($query);
+        /* Keep in mind the order of your variable in execute() method */
+        /* The order of the variable must be the same in your query, in this case the query is in line 19 */
         $result->execute([$username, password_hash($password, PASSWORD_DEFAULT), $email, $first_name, $last_name]);
 
         if ($result) {
